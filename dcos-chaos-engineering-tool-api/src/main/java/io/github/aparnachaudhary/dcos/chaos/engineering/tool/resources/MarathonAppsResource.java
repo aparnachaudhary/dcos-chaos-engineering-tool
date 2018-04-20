58df075a505e464da82f92c2a9a882065efc5008
Aparna Chaudhary <aparna.chaudhary@asml.com>
2018-04-20 16:04:55 +0200

    Added API for retrieval of chaos entity.

--- dcos-chaos-engineering-tool-api/src/main/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/resources/MarathonAppsResource.java
+++ dcos-chaos-engineering-tool-api/src/main/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/resources/MarathonAppsResource.java
@@ -3,8 +3,12 @@ package io.github.aparnachaudhary.dcos.chaos.engineering.tool.resources;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -15,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import io.github.aparnachaudhary.dcos.chaos.engineering.tool.entities.MarathonApplicationEntity;
+import io.github.aparnachaudhary.dcos.chaos.engineering.tool.exceptions.DcosChaosException;
 import io.github.aparnachaudhary.dcos.chaos.engineering.tool.services.MarathonAppChaosService;
 
 /**
@@ -47,4 +52,25 @@ public class MarathonAppsResource {
         return Response.created(location).build();
     }
 
+
+    @Path("{identifier}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createOptInApplication(@PathParam("identifier") final String identifier, MarathonApplicationEntity marathonApplication,
+                                           @Context final UriInfo uriInfo) throws DcosChaosException {
+        throw new DcosChaosException("NotYetImplemented");
+    }
+
+    @Path("{identifier}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getApplication(@PathParam("identifier") final String identifier) {
+        try {
+            MarathonApplicationEntity applicationEntity = marathonAppChaosService.getById(identifier);
+            return Response.ok(applicationEntity).build();
+        } catch (DcosChaosException e) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 }
--- dcos-chaos-engineering-tool-api/src/main/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/services/MarathonAppChaosService.java
+++ dcos-chaos-engineering-tool-api/src/main/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/services/MarathonAppChaosService.java
@@ -2,12 +2,14 @@ package io.github.aparnachaudhary.dcos.chaos.engineering.tool.services;
 
 import java.time.Instant;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import io.github.aparnachaudhary.dcos.chaos.engineering.tool.entities.MarathonApplicationEntity;
+import io.github.aparnachaudhary.dcos.chaos.engineering.tool.exceptions.DcosChaosException;
 import io.github.aparnachaudhary.dcos.chaos.engineering.tool.repositories.MarathonApplicationRepository;
 
 /**
@@ -35,5 +37,14 @@ public class MarathonAppChaosService {
             entity.setLastModifiedOn(Instant.now());
         }
         return applicationRepository.save(entity);
+    }
+
+    public MarathonApplicationEntity getById(String identifier) throws DcosChaosException {
+        final Optional<MarathonApplicationEntity> entity = applicationRepository.findById(identifier);
+        if (entity.isPresent()) {
+            return entity.get();
+        } else {
+            throw new DcosChaosException("Failed to retrieve the entity with identifier=" + identifier);
+        }
     }
 }
