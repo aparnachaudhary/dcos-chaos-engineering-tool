306d029af07ffd063b0def6a984c6f35f76531f1
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2018-04-20 08:50:53 +0200

    project skeleton

---
+++ dcos-chaos-engineering-tool-api/src/main/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/DcosChaosEngineeringToolApplication.java
@@ -0,0 +1,12 @@
+package io.github.aparnachaudhary.dcos.chaos.engineering.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcosChaosEngineeringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcosChaosEngineeringToolApplication.class, args);
+	}
+}
---
+++ dcos-chaos-engineering-tool-api/src/test/java/io/github/aparnachaudhary/dcos/chaos/engineering/tool/DcosChaosEngineeringToolApplicationTest.java
@@ -0,0 +1,20 @@
+package io.github.aparnachaudhary.dcos.chaos.engineering.tool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test for {@link DcosChaosEngineeringToolApplication}
+ *
+ * @author Aparna Chaudhary
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DcosChaosEngineeringToolApplicationTest {
+
+    @Test public void contextLoads() {
+    }
+
+}
